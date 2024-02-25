0fa424547f55de9bdb8ad6d182457e3b59587bd9
Charlie Yu <imo.charlie@gmail.com>
2024-02-25 17:19:06 +0000

    route for user signup

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const users_controller = require('./../../controllers/userController')
+
+router.post('/signup', users_controller.createUser)
+
+
 // TODO: auth
 // TODO: users_controller
 // TODO: users_validation
 
 // TODO: routes and controllers
+
+module.exports = router;
