1c8c84ee0cdfdcf89f03b2324ca70c2de5a3d01e
celia103 <celiayych@gmail.com>
2024-03-07 00:32:21 +0000

    Setup Signup page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,71 +1,83 @@
 //Register page
+import "../components/styles/register.css";
 import React, { useState } from "react";
+import { Form, Button, Card } from "react-bootstrap";
 
 function Register() {
+	// states
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [verifyPassword, setVerifyPassword] = useState("");
 
-  // states
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [verifyPassword, setVerifyPassword] = useState("")
+	// handleLogin
+	// may change to pass from parent component
+	const handleLogin = () => {
+		// TODO
+	};
 
-  // handleLogin
-  // may change to pass from parent component
-  const handleLogin = () => {
-    // TODO
-  }
+	return (
+		<div className="mood-register">
+			<div className="register-card">
+				<Form>
+					<h3>Register Form</h3>
+					<Form.Group className="mt-4 mb-4" controlId="registerForm.ControlInput1">
+						<Form.Label>Username</Form.Label>
+						<Form.Control
+							type="text"
+							placeholder="Enter your username"
+							value={username}
+							onChange={(event) => setUsername(event.target.value)}
+						/>
+						{/* Error message for username */}
+						<Form.Text className="text-error">This field cannot be empty.</Form.Text>
+					</Form.Group>
 
-  return (
-    <div>
-      <h1>Register Page</h1>
-      <div>
-        <h2>Register Form</h2>
-        <form onSubmit={handleLogin}>
-          <div>
-            Username
-            <input 
-              type="text" 
-              id="username"
-              value={username}
-              name="Username"
-              onChange={({target}) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            Email
-            <input 
-              type="text" 
-              id="email"
-              value={email}
-              name="Email"
-              onChange={({target}) => setEmail(target.value)}
-            />
-          </div>
-          <div>
-            Password
-            <input 
-              type="password" 
-              id="password"
-              value={password}
-              name="Password"
-              onChange={({target}) => setPassword(target.value)}
-            />
-          </div>
-          <div>
-            Verify Password
-            <input 
-              type="password" 
-              id="verify-password"
-              value={verifyPassword}
-              name="Verify Password"
-              onChange={({target}) => setVerifyPassword(target.value)}
-            />
-          </div>
-          <button id="register-button" type="submit">Register</button>
-        </form>
-      </div>
-    </div>
-  );
+					<Form.Group className="mb-4" controlId="registerForm.ControlInput2">
+						<Form.Label>Email address</Form.Label>
+						<Form.Control
+							type="email"
+							placeholder="Enter your email"
+							value={email}
+							onChange={({ target }) => setEmail(target.value)}
+						/>
+            {/* Error message for username */}
+						<Form.Text className="text-error"></Form.Text>
+					</Form.Group>
+
+					<Form.Group className="mb-4" controlId="registerForm.ControlInput3">
+						<Form.Label>Password</Form.Label>
+						<Form.Control
+							type="password"
+							placeholder="Enter your password"
+							value={password}
+							onChange={({ target }) => setPassword(target.value)}
+						/>
+            {/* Error message for username */}
+						<Form.Text className="text-error"></Form.Text>
+					</Form.Group>
+
+					<Form.Group className="mb-4" controlId="registerForm.ControlInput4">
+						<Form.Label>Verify Password</Form.Label>
+						<Form.Control
+							type="password"
+							placeholder="Confirm password"
+							value={verifyPassword}
+							onChange={({ target }) => setVerifyPassword(target.value)}
+						/>
+            {/* Error message for username */}
+						<Form.Text className="text-error"></Form.Text>
+					</Form.Group>
+          
+					<div className="d-grid mt-5">
+						<Button type="submit" className="register-btn">
+							Register
+						</Button>
+					</div>
+				</Form>
+			</div>
+		</div>
+	);
 }
 
 export default Register;
