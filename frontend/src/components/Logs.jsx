ac3504d8773db55c235f8ae0a2819d0cd293226c
celia103 <celiayych@gmail.com>
2024-03-11 23:17:00 +0000

    basic setup for dashboard

---
+++ frontend/src/components/Logs.jsx
@@ -0,0 +1,12 @@
+//Logs.jsx
+import React from "react";
+
+function Logs() {
+  return (
+    <div className="">
+      <h1>Logs</h1>
+    </div>
+  );
+}
+
+export default Logs;
---
+++ frontend/src/components/Mood.jsx
@@ -0,0 +1,12 @@
+//Mood.jsx
+import React from "react";
+
+function Mood() {
+  return (
+    <div className="">
+      <h1>Mood</h1>
+    </div>
+  );
+}
+
+export default Mood;
