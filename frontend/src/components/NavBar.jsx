374e1756d1bc33c6ae3a84ec18e442037437ec2f
celia103 <celiayych@gmail.com>
2024-03-11 15:55:10 +0000

    add dropdown to nav bar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,28 +2,28 @@
 import "./styles/NavBar.css";
 import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
-import { Navbar, Nav, Button } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 import logo from "/assets/images/MoodTrack_icon.png";
 
 import { AuthContext } from "../components/AuthContext";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
 	const [expanded, setExpanded] = useState(false);
 
 	const [errorMessage, setErrorMessage] = useState("");
 
-	const { user, setUser, setToken } = useContext(AuthContext)
-	const navigate = useNavigate()
+	const { user, setUser, setToken } = useContext(AuthContext);
+	const navigate = useNavigate();
 
 	const handleLogout = async (e) => {
-		e.preventDefault();
-		setToken(null)
-		setUser(null)
-		localStorage.removeItem('token')
-		navigate("/")
-	}
+		e.p;
+		setToken(null);
+		setUser(null);
+		localStorage.removeItem("token");
+		navigate("/");
+	};
 
 	return (
 		<Navbar expand="lg" className="mood-nav">
@@ -43,14 +43,6 @@ function NavBar() {
 							Home
 						</Nav.Link>
 
-						<Nav.Link as={Link} to="/dashboard" className="mood-nav-link">
-							Dashboard
-						</Nav.Link>
-
-						<Nav.Link as={Link} to="/log" className="mood-nav-link">
-							Log
-						</Nav.Link>
-
 						<Nav.Link as={Link} to="/resources" className="mood-nav-link">
 							Resources
 						</Nav.Link>
@@ -58,32 +50,53 @@ function NavBar() {
 						<Nav.Link as={Link} to="/about" className="mood-nav-link">
 							About
 						</Nav.Link>
+						<NavDropdown
+							title="Dashboard"
+							id="nav-dropdown"
+							className="mood-nav-link"
+						>
+							<NavDropdown.Item as={Link} to="/mood" className="mood-nav-dropdown">
+								Mood / Activities
+							</NavDropdown.Item>
+							<NavDropdown.Item as={Link} to="/journal" className="mood-nav-dropdown">
+								Journal
+							</NavDropdown.Item>
+							<NavDropdown.Item as={Link} to="/logs" className="mood-nav-dropdown">
+								Logs
+							</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item as={Link} to="/setting" className="mood-nav-dropdown">
+								Setting
+							</NavDropdown.Item>
+						</NavDropdown>
 					</Nav>
 
 					<Nav className="justify-content-end">
-						{!user && <Nav.Item>
-							<Nav.Link as={Link} to="/login" className="mood-nav-btn mood-nav-login">
-								Login
-							</Nav.Link>
-						</Nav.Item>}
-						{!user && <Nav.Item>
-							<Nav.Link
-								as={Link}
-								to="/register"
-								className="mood-nav-btn mood-nav-signup"
-							>
-								Signup
-							</Nav.Link>
-						</Nav.Item>}
-						{user && <Nav.Item onClick={handleLogout}>
-							<Nav.Link
-								as={Link}
-								to=""
-								className="mood-nav-btn mood-nav-logout"
-							>
-								Logout
-							</Nav.Link>
-						</Nav.Item>}
+						{!user && (
+							<Nav.Item>
+								<Nav.Link as={Link} to="/login" className="mood-nav-btn mood-nav-login">
+									Login
+								</Nav.Link>
+							</Nav.Item>
+						)}
+						{!user && (
+							<Nav.Item>
+								<Nav.Link
+									as={Link}
+									to="/register"
+									className="mood-nav-btn mood-nav-signup"
+								>
+									Signup
+								</Nav.Link>
+							</Nav.Item>
+						)}
+						{user && (
+							<Nav.Item onClick={handleLogout}>
+								<Nav.Link as={Link} to="" className="mood-nav-btn mood-nav-logout">
+									Logout
+								</Nav.Link>
+							</Nav.Item>
+						)}
 					</Nav>
 				</Navbar.Collapse>
 			</div>
