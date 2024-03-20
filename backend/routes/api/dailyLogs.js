dd6e4c2165fa26bd665fc7113d15ce4aa0405a30
Charlie Yu <imo.charlie@gmail.com>
2024-03-20 13:52:26 +0000

    completed saveLog route and controller

--- backend/routes/api/dailyLogs.js
+++ backend/routes/api/dailyLogs.js
@@ -2,13 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { auth } = require('./../../middlewares/auth')
 const dailyLogController = require('./../../controllers/dailyLogController')
-// TODO: maybe add validation for log updates
 
-router.post('/mood', auth, dailyLogController.saveMoodLog)
-
-// router.post('/sleep', auth, dailyLogController.saveSleepLog)
-
-// router.post('/activities', auth, dailyLogController.saveActivityLog)
+router.post('/saveLog', auth, dailyLogController.saveLog)
 
 // router.get('/logs', auth, dailyLogController.fetchLogs)
 
