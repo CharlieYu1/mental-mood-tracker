b86ee271b118ebf710065b93b00f82ec49ce8e0c
celia103 <celiayych@gmail.com>
2024-03-30 12:16:37 +0000

    Update time picker in logs

--- frontend/src/components/Logs.jsx
+++ frontend/src/components/Logs.jsx
@@ -1,16 +1,26 @@
 import React, { useState } from "react";
-import {
-	Container,
-	Row,
-	Card,
-	Button,
-	Col,
-	Form,
-	InputGroup,
-} from "react-bootstrap";
+import { Container, Row, Card, Button, Col, Form } from "react-bootstrap";
 import dayjs from "dayjs";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
+import { faClockFour } from "@fortawesome/free-regular-svg-icons";
+
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import { makeStyles } from "@mui/styles";
+
+// const useStyles = makeStyles({
+// 	root: {
+// 		"& .MuiOutlinedInput-notchedOutline": {
+// 			borderColor: "yellow",
+// 		},
+// 		"&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+// 			borderColor: "yellow",
+// 		},
+// 	},
+// });
 
 import icon1 from "/assets/images/mood-icon-01.png";
 import icon10 from "/assets/images/mood-icon-05.png";
@@ -36,6 +46,11 @@ function Logs() {
 		formattedDate = currentDate.format("DD/MM/YYYY");
 	}
 
+	// Time picker
+	const today = dayjs();
+	const todayStartOfTheDay = today.startOf("day");
+	// const classes = useStyles();
+
 	return (
 		<Container className="content col-10">
 			{/* Date */}
@@ -56,16 +71,14 @@ function Logs() {
 					</h2>
 				</Col>
 			</Row>
-			
+
 			{/* Button */}
 			<Row className="justify-content-center">
-				<Button className="save-btn">
-					Save
-				</Button>
+				<Button className="save-btn">Save</Button>
 			</Row>
 
 			{/* Dashboard cards - Rate Mood */}
-			<Row className="d-md-flex flex-md-equal my-md-3 mood-row">
+			<Row className="d-md-flex flex-md-equal my-3 mood-row">
 				<Card className="shadow-sm h-100 px-3 pt-3 pb-1 align-items-center">
 					<Card.Title className="dashboard-card-title log-title">
 						How would you rate your mood?
@@ -81,12 +94,55 @@ function Logs() {
 							<img src={icon10} className="icon-img" alt="Image 10" />
 						</div>
 					</Card.Body>
-					<Card.Body
-						className="w-100"
-					>
+					<Card.Body className="w-100">
+						<Form.Group className="w-100">
+							<Form.Control
+								as="textarea"
+								rows={3}
+								placeholder="What made you feel good or bad today? Add note... (Optional)"
+							/>
+						</Form.Group>
+					</Card.Body>
+				</Card>
+			</Row>
+
+			<Row className="d-md-flex flex-md-equal my-3 mood-row">
+				<Card className="shadow-sm h-100 px-3 pt-3 pb-1 align-items-center">
+					<Card.Title className="dashboard-card-title log-title">
+						Sleeping Log
+					</Card.Title>
+
+					<Card.Body className="w-100">
+						<Container className="d-flex w-100 justify-content-center">
+						<Row className="w-100">
+							<Col xs={12} md={4}>
+								<LocalizationProvider dateAdapter={AdapterDayjs}>
+									<DemoContainer components={["TimePicker"]}>
+										<DemoItem label="Time Went to Bed">
+											<TimePicker defaultValue={todayStartOfTheDay} disablePast />
+										</DemoItem>
+									</DemoContainer>
+								</LocalizationProvider>
+							</Col>
+							<Col xs={12} md={4}>
+								<LocalizationProvider dateAdapter={AdapterDayjs}>
+									<DemoContainer components={["TimePicker"]}>
+										<DemoItem label="Time Woke up">
+											<TimePicker defaultValue={todayStartOfTheDay} disablePast />
+										</DemoItem>
+									</DemoContainer>
+								</LocalizationProvider>
+							</Col>
+							<Col xs={12} md={4}>
+								<div>Duration: hr</div>
+							</Col>
+						</Row></Container>
+					</Card.Body>
+
+					<Card.Body className="w-100">
 						<Form.Group className="w-100">
 							{/* <Form.Label>Example textarea</Form.Label> */}
-							<Form.Control as="textarea" rows={3} placeholder="What made you feel good or bad today? Add note... (Optional)" />
+							<Form.Control as="textarea" rows={3} placeholder="Note... (Optional)" />
 						</Form.Group>
 					</Card.Body>
 				</Card>
