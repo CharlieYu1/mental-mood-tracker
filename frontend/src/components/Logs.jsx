c7426537bd397d1d6e7b85fd3b19a429bac2bc7a
celia103 <celiayych@gmail.com>
2024-03-31 03:36:12 +0100

    Update log page

--- frontend/src/components/Logs.jsx
+++ frontend/src/components/Logs.jsx
@@ -2,14 +2,33 @@ import React, { useState } from "react";
 import { Container, Row, Card, Button, Col, Form } from "react-bootstrap";
 import dayjs from "dayjs";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
-import { faClockFour } from "@fortawesome/free-regular-svg-icons";
+import {
+	faCaretLeft,
+	faCaretRight,
+	faHouseChimneyWindow,
+	faUserGroup,
+	faHeart,
+	faComputer,
+	faPalette,
+	faVolleyball,
+	faScrewdriverWrench,
+	faGraduationCap,
+	faPersonWalkingLuggage,
+	faPersonRunning,
+	faDumbbell,
+	faSpa,
+	faPersonBiking,
+	faPersonWalking,
+	faMugSaucer,
+	faUtensils,
+	faBellConcierge,
+} from "@fortawesome/free-solid-svg-icons";
+import { faComment, faFaceSmile } from "@fortawesome/free-regular-svg-icons";
 
 import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { TimePicker } from "@mui/x-date-pickers/TimePicker";
-import { makeStyles } from "@mui/styles";
 
 // const useStyles = makeStyles({
 // 	root: {
@@ -106,6 +125,7 @@ function Logs() {
 				</Card>
 			</Row>
 
+			{/* Dashboard cards - Sleeping Log */}
 			<Row className="d-md-flex flex-md-equal my-3 mood-row">
 				<Card className="shadow-sm h-100 px-3 pt-3 pb-1 align-items-center">
 					<Card.Title className="dashboard-card-title log-title">
@@ -113,37 +133,503 @@ function Logs() {
 					</Card.Title>
 
 					<Card.Body className="w-100">
-						<Container className="d-flex w-100 justify-content-center">
-						<Row className="w-100">
-							<Col xs={12} md={4}>
-								<LocalizationProvider dateAdapter={AdapterDayjs}>
-									<DemoContainer components={["TimePicker"]}>
-										<DemoItem label="Time Went to Bed">
-											<TimePicker defaultValue={todayStartOfTheDay} disablePast />
-										</DemoItem>
-									</DemoContainer>
-								</LocalizationProvider>
+						<Container className="d-flex w-100 justify-content-center px-0">
+							<Row className="w-100">
+								<Col xs={12} md={6} lg={4}>
+									<LocalizationProvider dateAdapter={AdapterDayjs}>
+										<DemoContainer components={["TimePicker"]}>
+											<DemoItem label="Time Went to Bed">
+												<TimePicker
+													defaultValue={todayStartOfTheDay}
+													disablePast
+													// slotProps={{
+													// 	textField: {
+													// 		sx: {
+													// 			"& .MuiOutlinedInput-root": {
+													// 				width: 150,
+													// 			},
+													// 		},
+													// 	},
+													// }}
+												/>
+											</DemoItem>
+										</DemoContainer>
+									</LocalizationProvider>
+								</Col>
+
+								<Col xs={12} md={6} lg={4}>
+									<LocalizationProvider dateAdapter={AdapterDayjs}>
+										<DemoContainer components={["TimePicker"]}>
+											<DemoItem label="Time Woke up">
+												<TimePicker defaultValue={todayStartOfTheDay} disablePast />
+											</DemoItem>
+										</DemoContainer>
+									</LocalizationProvider>
+								</Col>
+								<Col xs={12} md={12} lg={4}>
+									<div>Duration: hr</div>
+								</Col>
+							</Row>
+						</Container>
+					</Card.Body>
+
+					<Card.Body className="d-flex flex-column w-100">
+						<Row className="sleep-btn w-100">
+							<Col lg={2}>
+								<Card.Text>Quality:</Card.Text>
 							</Col>
-							<Col xs={12} md={4}>
-								<LocalizationProvider dateAdapter={AdapterDayjs}>
-									<DemoContainer components={["TimePicker"]}>
-										<DemoItem label="Time Woke up">
-											<TimePicker defaultValue={todayStartOfTheDay} disablePast />
-										</DemoItem>
-									</DemoContainer>
-								</LocalizationProvider>
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="align-items-center text-center px-0"
+							>
+								<Button className="quality-btn-5">Excellent</Button>
 							</Col>
-							<Col xs={12} md={4}>
-								<div>Duration: hr</div>
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="align-items-center text-center px-0"
+							>
+								<Button className="quality-btn-4">Good</Button>
 							</Col>
-						</Row></Container>
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="align-items-center text-center px-0"
+							>
+								<Button className="quality-btn-3">Fair</Button>
+							</Col>
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="align-items-center text-center px-0"
+							>
+								<Button className="quality-btn-2">Poor</Button>
+							</Col>
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="align-items-center text-center px-0"
+							>
+								<Button className="quality-btn-1">Very Poor</Button>
+							</Col>
+						</Row>
 					</Card.Body>
 
 					<Card.Body className="w-100">
 						<Form.Group className="w-100">
-							{/* <Form.Label>Example textarea</Form.Label> */}
 							<Form.Control as="textarea" rows={3} placeholder="Note... (Optional)" />
 						</Form.Group>
+					</Card.Body>
+				</Card>
+			</Row>
+
+			{/* Dashboard cards - Activities Log */}
+			<Row className="d-md-flex flex-md-equal my-3 mood-row">
+				<Card className="shadow-sm h-100 px-3 pt-3 pb-1 align-items-center">
+					<Card.Title className="dashboard-card-title log-title">
+						Activities Log
+					</Card.Title>
+					<Card.Body className="d-flex flex-column w-100">
+						{/* Social icons */}
+						<Row className="act-btn w-100">
+							<Card.Title>Social:</Card.Title>
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-social">
+									<FontAwesomeIcon
+										icon={faHouseChimneyWindow}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Family Time</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-social">
+									<FontAwesomeIcon
+										icon={faUserGroup}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Friend Hangout</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-social">
+									<FontAwesomeIcon
+										icon={faHeart}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Date</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-social">
+									<FontAwesomeIcon
+										icon={faComment}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Social Events</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-social">
+									<FontAwesomeIcon
+										icon={faComputer}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Socializing Online</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-social">
+									<FontAwesomeIcon
+										icon={faFaceSmile}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Others</Card.Text>
+							</Col>
+						</Row>
+
+						{/* Hobbies icons */}
+						<Row className="act-btn w-100 mt-4">
+							<Card.Title>Hobbies:</Card.Title>
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-hobbies">
+									<FontAwesomeIcon
+										icon={faPalette}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Arts/ Music</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-hobbies">
+									<FontAwesomeIcon
+										icon={faVolleyball}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Sports</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-hobbies">
+									<FontAwesomeIcon
+										icon={faScrewdriverWrench}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>DIY</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-hobbies">
+									<FontAwesomeIcon
+										icon={faGraduationCap}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Learning</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-hobbies">
+									<FontAwesomeIcon
+										icon={faPersonWalkingLuggage}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Travel</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-hobbies">
+									<FontAwesomeIcon
+										icon={faFaceSmile}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Others</Card.Text>
+							</Col>
+						</Row>
+
+						{/* Exercises icons */}
+						<Row className="act-btn w-100 mt-4">
+							<Card.Title>Exercises:</Card.Title>
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-exercise">
+									<FontAwesomeIcon
+										icon={faPersonRunning}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Jogging</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-exercise">
+									<FontAwesomeIcon
+										icon={faDumbbell}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Gym</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-exercise">
+									<FontAwesomeIcon
+										icon={faSpa}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Yoga</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-exercise">
+									<FontAwesomeIcon
+										icon={faPersonBiking}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Cycling</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-exercise">
+									<FontAwesomeIcon
+										icon={faPersonWalking}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Walking</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-exercise">
+									<FontAwesomeIcon
+										icon={faFaceSmile}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Others</Card.Text>
+							</Col>
+						</Row>
+
+						{/* Good Meals icons */}
+						<Row className="act-btn w-100 mt-4">
+							<Card.Title>Good Meals:</Card.Title>
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-meal">
+									<FontAwesomeIcon
+										icon={faMugSaucer}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Breakfast</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-meal">
+									<FontAwesomeIcon
+										icon={faUtensils}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Lunch</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-meal">
+									<FontAwesomeIcon
+										icon={faBellConcierge}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Dinner</Card.Text>
+							</Col>
+
+							<Col
+								xs={6}
+								sm={3}
+								md={4}
+								lg={2}
+								className="p-0 align-items-center text-center"
+							>
+								<Button className="rounded-circle act-btn-meal">
+									<FontAwesomeIcon
+										icon={faFaceSmile}
+										className="act-btn-icon"
+										onClick={goToNextMonth}
+									/>
+								</Button>
+								<Card.Text>Others</Card.Text>
+							</Col>
+						</Row>
 					</Card.Body>
 				</Card>
 			</Row>
@@ -151,6 +637,7 @@ function Logs() {
 	);
 }
 
+// For rating button color
 function getColor(index) {
 	switch (index) {
 		case 0:
